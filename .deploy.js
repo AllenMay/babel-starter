e53cebfb0e0c7825295392c310b7a684b5f096b5
Allen May <allenhmay@gmail.com>
2016-10-01 10:42:31 -0400

    Clean up local

---
+++ .deploy.js
@@ -0,0 +1,8 @@
+// TO PUSH TO MayGourmet:
+// rsync -azt /Users/allen/Development/babel-starter/ maygourm@maygourmet.com:/home/maygourm/public_html/babel
+// 10thAuzzy
+
+
+// Brant and Vanessa
+// rsync -azt /Users/allen/Development/VanessaAndBrant/ maygourm@maygourmet.com:/home/maygourm/public_html/VBW
+// 10thAuzzy
